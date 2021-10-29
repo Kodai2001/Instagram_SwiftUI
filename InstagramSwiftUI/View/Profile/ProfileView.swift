223dac93142eac1bece53788eba8e86e2c2f4451
Kodai <system@Kodai-HayashinoMacBook.local>
2021-10-29 09:35:48 +0900

    first commit

---
+++ InstagramSwiftUI/View/Profile/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  InstagramSwiftUI
+//
+//  Created by system on 2021/10/29.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
